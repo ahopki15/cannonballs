4baf14a90e324ba3acf98892ec36c2f3c0589237
Austin Hopkins <hopkinsaustin@gmail.com>
2018-05-01 00:23:56 -0400

    Copied non-functionalized part of program and turned into a function (make_plots) that accepts a planet argument as either Earth or Mars. Gravity and air densities were moved from global variables to being initialized based on the planet chosen. Will run make_plots('Mars') to produce Martian plots

--- cannonballs.py
+++ cannonballs.py
@@ -4,9 +4,7 @@ import pylab as plt
 R = 0.08
 m = 1
 v0 = 100
-rho = 1.22
 C = 0.47
-g = 9.8
 v0x = (np.sqrt(3)/2)*v0
 v0y = (1/2)*v0
 x0 = 0
@@ -46,66 +44,137 @@ def estimate_zero(X):
     w_2 = -np.max(X[:,1][X[:,1]<0])/tot
     guess = w_1*X[index_1,0]+w_2*X[index_2,0]
     return guess
-
-t = [t0]
-X = [X0]
-for i in range(1,N_steps):
-    t.append(t[i-1]+h)
-    new_X = X[i-1]+delta_x_t_h(X[i-1],f)
-    X.append(new_X)
-t = np.array(t)
-X = np.array(X)
-
-fg, ax = plt.subplots(1,1,figsize=(12,8))
-plt.plot(X[:,0],X[:,1],lw=3)
-#plt.axhline(0,c='k',linestyle='--')
-plt.ylim(0,60)
-plt.xlabel('Horizontal Distance',fontsize=18)
-plt.ylabel('Height',fontsize=18)
-plt.title('Trajectory of 1kg Cannon Ball',fontsize=20)
-plt.tick_params(axis='both',labelsize=16)
-plt.show()
 	
-masses = np.linspace(.25,8,20)
-zeros = []
-for m in masses:
-    t = [t0]
-    X = [X0]
-    i = 1
-    while (i < N_steps and X[-1][1])>=0:
-        t.append(t[i-1]+h)
-        new_X = X[i-1]+delta_x_t_h(X[i-1],f)
-        X.append(new_X)
-        i = i + 1
-    t = np.array(t)
-    X = np.array(X)
-    zeros.append(estimate_zero(X))
-	
-fg, ax = plt.subplots(1,1,figsize=(12,8))
-masses3 = [1,2,4,8]
-for m in masses3:
-    t = [t0]
-    X3 = [X0]
-    for i in range(1,N_steps):
-        t.append(t[i-1]+h)
-        new_X = X3[i-1]+delta_x_t_h(X3[i-1],f)
-        X3.append(new_X)
-    t = np.array(t)
-    X3 = np.array(X3)
-    plt.plot(X3[:,0],X3[:,1],lw=3,label='m='+str(m)+' kg')
-plt.xlabel('Horizontal Distance',fontsize=18)
-plt.ylabel('Height',fontsize=18)
-plt.title('Trajectories of Cannon Balls of Various Masses',fontsize=20)
-plt.tick_params(axis='both',labelsize=16)
-plt.legend(fontsize=18)
-plt.ylim(0,150)
-plt.xlim(-10,650)
-plt.show()
+def make_plots(planet):
+	if planet == 'Earth':
+		rho = 1.22
+		g = 9.8
+		t = [t0]
+		X = [X0]
+		for i in range(1,N_steps):
+			t.append(t[i-1]+h)
+			new_X = X[i-1]+delta_x_t_h(X[i-1],f)
+			X.append(new_X)
+		t = np.array(t)
+		X = np.array(X)
 
-fg, ax = plt.subplots(1,1,figsize=(12,8))
-plt.scatter(masses,zeros,lw=3)
-plt.xlabel('Mass (kg)',fontsize=18)
-plt.ylabel('Distance Travelled (m)',fontsize=18)
-plt.title('Distance Travelled of Cannonballs of Different Masses',fontsize=20)
-plt.tick_params(axis='both',labelsize=16)
-plt.show()
+		fg, ax = plt.subplots(1,1,figsize=(12,8))
+		plt.plot(X[:,0],X[:,1],lw=3)
+		#plt.axhline(0,c='k',linestyle='--')
+		plt.ylim(0,60)
+		plt.xlabel('Horizontal Distance',fontsize=18)
+		plt.ylabel('Height',fontsize=18)
+		plt.title('Trajectory of 1kg Cannon Ball on Earth',fontsize=20)
+		plt.tick_params(axis='both',labelsize=16)
+		plt.show()
+			
+		masses = np.linspace(.25,8,20)
+		zeros = []
+		for m in masses:
+			t = [t0]
+			X = [X0]
+			i = 1
+			while (i < N_steps and X[-1][1])>=0:
+				t.append(t[i-1]+h)
+				new_X = X[i-1]+delta_x_t_h(X[i-1],f)
+				X.append(new_X)
+				i = i + 1
+			t = np.array(t)
+			X = np.array(X)
+			zeros.append(estimate_zero(X))
+			
+		fg, ax = plt.subplots(1,1,figsize=(12,8))
+		masses3 = [1,2,4,8]
+		for m in masses3:
+			t = [t0]
+			X3 = [X0]
+			for i in range(1,N_steps):
+				t.append(t[i-1]+h)
+				new_X = X3[i-1]+delta_x_t_h(X3[i-1],f)
+				X3.append(new_X)
+			t = np.array(t)
+			X3 = np.array(X3)
+			plt.plot(X3[:,0],X3[:,1],lw=3,label='m='+str(m)+' kg')
+		plt.xlabel('Horizontal Distance',fontsize=18)
+		plt.ylabel('Height',fontsize=18)
+		plt.title('Trajectories of Cannon Balls of Various Masses on Earth',fontsize=20)
+		plt.tick_params(axis='both',labelsize=16)
+		plt.legend(fontsize=18)
+		plt.ylim(0,150)
+		plt.xlim(-10,650)
+		plt.show()
+
+		fg, ax = plt.subplots(1,1,figsize=(12,8))
+		plt.scatter(masses,zeros,lw=3)
+		plt.xlabel('Mass (kg)',fontsize=18)
+		plt.ylabel('Distance Travelled (m)',fontsize=18)
+		plt.title('Distance Travelled of Cannonballs of Different Masses on Earth',fontsize=20)
+		plt.tick_params(axis='both',labelsize=16)
+		plt.show()
+	elif planet == 'Mars':
+		rho = 0.20
+		g = 3.71
+		t = [t0]
+		X = [X0]
+		for i in range(1,N_steps):
+			t.append(t[i-1]+h)
+			new_X = X[i-1]+delta_x_t_h(X[i-1],f)
+			X.append(new_X)
+		t = np.array(t)
+		X = np.array(X)
+
+		fg, ax = plt.subplots(1,1,figsize=(12,8))
+		plt.plot(X[:,0],X[:,1],lw=3)
+		#plt.axhline(0,c='k',linestyle='--')
+		plt.ylim(0,60)
+		plt.xlabel('Horizontal Distance',fontsize=18)
+		plt.ylabel('Height',fontsize=18)
+		plt.title('Trajectory of 1kg Cannon Ball on Earth',fontsize=20)
+		plt.tick_params(axis='both',labelsize=16)
+		plt.show()
+			
+		masses = np.linspace(.25,8,20)
+		zeros = []
+		for m in masses:
+			t = [t0]
+			X = [X0]
+			i = 1
+			while (i < N_steps and X[-1][1])>=0:
+				t.append(t[i-1]+h)
+				new_X = X[i-1]+delta_x_t_h(X[i-1],f)
+				X.append(new_X)
+				i = i + 1
+			t = np.array(t)
+			X = np.array(X)
+			zeros.append(estimate_zero(X))
+			
+		fg, ax = plt.subplots(1,1,figsize=(12,8))
+		masses3 = [1,2,4,8]
+		for m in masses3:
+			t = [t0]
+			X3 = [X0]
+			for i in range(1,N_steps):
+				t.append(t[i-1]+h)
+				new_X = X3[i-1]+delta_x_t_h(X3[i-1],f)
+				X3.append(new_X)
+			t = np.array(t)
+			X3 = np.array(X3)
+			plt.plot(X3[:,0],X3[:,1],lw=3,label='m='+str(m)+' kg')
+		plt.xlabel('Horizontal Distance',fontsize=18)
+		plt.ylabel('Height',fontsize=18)
+		plt.title('Trajectories of Cannon Balls of Various Masses on Earth',fontsize=20)
+		plt.tick_params(axis='both',labelsize=16)
+		plt.legend(fontsize=18)
+		plt.ylim(0,150)
+		plt.xlim(-10,650)
+		plt.show()
+
+		fg, ax = plt.subplots(1,1,figsize=(12,8))
+		plt.scatter(masses,zeros,lw=3)
+		plt.xlabel('Mass (kg)',fontsize=18)
+		plt.ylabel('Distance Travelled (m)',fontsize=18)
+		plt.title('Distance Travelled of Cannonballs of Different Masses on Earth',fontsize=20)
+		plt.tick_params(axis='both',labelsize=16)
+		plt.show()
+		
+make_plots('Mars')
